5fc87b364a2b7dbe2e128422628503472c4580da
Evan Winslow <evan@elgg.org>
2011-06-17 01:40:40 -0700

    Checking permissions before outputting add answer form on question page

--- pages/questions/view.php
+++ pages/questions/view.php
@@ -36,12 +36,14 @@ $count = elgg_get_entities($options);
 
 $content .= elgg_view_module('info', "$count " . elgg_echo('answers'), elgg_view_menu('filter') . $answers);
 
-$user_icon = elgg_view_entity_icon(elgg_get_logged_in_user_entity(), 'small');
-$add_form = elgg_view_form('object/answer/add', array(), array('container_guid' => $question->guid));
-
-$add_block = elgg_view_image_block($user_icon, $add_form);
-
-$content .= elgg_view_module('info', elgg_echo('answers:addyours'), $add_block);
+if ($question->canWriteToContainer(0, 'object', 'answer')) {
+	$user_icon = elgg_view_entity_icon(elgg_get_logged_in_user_entity(), 'small');
+	$add_form = elgg_view_form('object/answer/add', array(), array('container_guid' => $question->guid));
+	
+	$add_block = elgg_view_image_block($user_icon, $add_form);
+	
+	$content .= elgg_view_module('info', elgg_echo('answers:addyours'), $add_block);
+}
 
 $body = elgg_view_layout('content', array(
 	'title' => $title,
