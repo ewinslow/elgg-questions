4aa56a19220e19ee93538e2ba79de2c45710b10c
Liang Lee ( Sunny ) <manactric@gmail.com>
2012-05-22 10:50:29 +0600

    Update languages/pt_br.php

--- languages/pt_br.php
+++ languages/pt_br.php
@@ -8,7 +8,7 @@ add_translation('pt_br', array(
 'item:object:question' => "Perguntas",
 'questions' => 'Perguntar',
 'questions:asked' => 'Perguntado por %s',
-'questions:answered' => 'Última resposta por %s %s',
+'questions:answered' => 'ï¿½ltima resposta por %s %s',
 'questions:everyone' => 'Todas as perguntas',
 'questions:add' => 'Perguntar',
 'questions:owner' => "Perguntas de %s",
@@ -19,8 +19,8 @@ add_translation('pt_br', array(
 'object:question:description' => "Detalhes",
 'questions:river:question:created:by' => "Nova pergunta criada por",
 'question:view' => " Visualizar",
-'questions:river:question:created:by' => "%s Posted new Question.",
-'questions:river:answer:created:by' => "%s posted Answer of Question",
+'questions:river:question:created:by' => "%s Postado nova pergunta.",
+'questions:river:answer:created:by' => "%s Resposta postada da pergunta",
 'question:view' => " view",
 
 /**
@@ -28,7 +28,7 @@ add_translation('pt_br', array(
 */
 
 'widget:questions:title' => "Perguntas",
-'widget:questions:description' => "Você pode ver o status de suas perguntas.",
-'widget:questions:limit' => "Número de perguntas para exibir:",
+'widget:questions:description' => "Vocï¿½ pode ver o status de suas perguntas.",
+'widget:questions:limit' => "Nï¿½mero de perguntas para exibir:",
 
 ));
